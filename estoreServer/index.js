3f5bc816d92c7dd4adfbbae12ac0cbcde806d402
Pooja Bhakta <pjmira19@gmail.com>
2023-08-22 12:53:26 -0400

    server

---
+++ estoreServer/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const productCategories = require('./Routes/productCategories');
+const app = express();
+
+app.use("/productCategories", productCategories)
+
+
+
+
+
+const PORT = 5001;
+ 
+
+const server = app.listen(PORT, ()=>{
+  console.log("App is running on the port - 5001")
+})
